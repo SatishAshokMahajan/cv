40e582d6990724916c3dbcb541a0abfe89bf351a
Satish Mahajan <satishmahajan29@gmail.com>
2019-09-15 13:29:26 +0530

    Basic CRA with SCSS boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Basic CRA with SCSS boilerplate
+    </div>
+  );
+}
+
+export default App;
